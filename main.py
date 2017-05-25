882d549b4a6343d542c66f73d07f87d86d823d2a
Tom Eaton <thomfurpwns@gmail.com>
2017-05-25 22:34:33 +0100

    Added dynamic update of tape and pointer :satisfied:

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+import os
+import time
 tape = [1, 1, 1, 'B']
 state = 'q0'
 index = 0
-print(index)
 
-def print_tape(name):
-    print(*name, sep='')
-
-def print_arrow(index):
+def print_arrow_tape(name, index):
     spaces = ''
+    tape_string = ''
     for i in range(0,index):
         spaces += ' '
+
+    for i in name:
+        if i == 1 or i == 0:
+            tape_string += str(i)
+
+    os.system('cls')
+    print(tape_string)
     print(spaces + '^')
+
+print_arrow_tape(tape, index)
+time.sleep(1)
 
 while state != 'qf':
     if(state == 'q0'):
         if(tape[index] == 1):
             index += 1
-            print_tape(tape)
-            print_arrow(4)
+            print_arrow_tape(tape,index)
         elif(tape[index] == 'B'):
             state = 'qf'
+        time.sleep(1)
 
-print('done')
+
