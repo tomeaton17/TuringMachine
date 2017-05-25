8a238fbdcf16868e497fa307d94d683cfb8fc69a
Tom Eaton <thomfurpwns@gmail.com>
2017-05-25 21:50:00 +0100

    Added printing of tape and index arrow

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ state = 'q0'
 index = 0
 print(index)
 
+def print_tape(name):
+    print(*name, sep='')
+
+def print_arrow(index):
+    spaces = ''
+    for i in range(0,index):
+        spaces += ' '
+    print(spaces + '^')
+
 while state != 'qf':
     if(state == 'q0'):
         if(tape[index] == 1):
             index += 1
-            print(index)
+            print_tape(tape)
+            print_arrow(4)
         elif(tape[index] == 'B'):
             state = 'qf'
 
